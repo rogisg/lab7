4c76ff5afa01eaeb18a798ee25987ecdc106adec
rogisg <gogalebedev91@gmail.com>
2025-04-26 12:15:00 +0300

    task.cpp

--- task.cpp
+++ task.cpp
@@ -1,132 +1,190 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <map>
+#include <utility>
 
 using namespace std;
 
-// Задание 1
-void saveToFile(const string& filename, const vector<string>& data) {
-    ofstream file(filename);
-    for (const auto& word : data) {
-        file << word << endl;
+// Часть 1: Реализация unique_ptr
+template<class T>
+class MyUnique {
+    T* p = nullptr;
+
+public:
+    // Конструктор с сырым указателем
+    explicit MyUnique(T* ptr = nullptr) : p(ptr) {}
+    
+    // Запрет копирования
+    MyUnique(const MyUnique&) = delete;
+    MyUnique& operator=(const MyUnique&) = delete;
+    
+    // Перемещение
+    MyUnique(MyUnique&& other) noexcept : p(other.p) {
+        other.p = nullptr;
     }
-}
-
-void loadFromFile(const string& filename, vector<string>& outData) {
-    ifstream file(filename);
-    string word;
-    while (getline(file, word)) {
-        outData.push_back(word);
-    }
-}
-
-// Задание 2
-struct Book {
-    string Author;
-    string Title;
-    int Year;
-};
-
-void saveToFile(const string& filename, const vector<Book>& data) {
-    ofstream file(filename);
-    for (const auto& book : data) {
-        file << book.Author << endl;
-        file << book.Title << endl;
-        file << book.Year << endl;
-    }
-}
-
-void loadFromFile(const string& filename, vector<Book>& outData) {
-    ifstream file(filename);
-    Book book;
-    while (getline(file, book.Author) && 
-           getline(file, book.Title) && 
-           (file >> book.Year)) {
-        outData.push_back(book);
-        file.ignore();
-    }
-}
-
-// Задание 3
-enum Score {
-    Unsatisfactorily = 2,
-    Satisfactorily,
-    Good,
-    Excellent
-};
-
-struct Student {
-    string Name;
-    int Year;
-    map<string, Score> RecordBook;
-};
-
-using Groups = map<string, vector<Student>>;
-
-void saveToFile(const string& filename, const Groups& groups) {
-    ofstream file(filename);
-    for (const auto& group : groups) {
-        file << group.first << endl;
-        file << group.second.size() << endl;
-        for (const auto& student : group.second) {
-            file << student.Name << endl;
-            file << student.Year << endl;
-            file << student.RecordBook.size() << endl;
-            for (const auto& record : student.RecordBook) {
-                file << record.first << endl;
-                file << record.second << endl;
-            }
+    
+    MyUnique& operator=(MyUnique&& other) noexcept {
+        if (this != &other) {
+            delete p;
+            p = other.p;
+            other.p = nullptr;
         }
+        return *this;
     }
+    
+    // Деструктор
+    ~MyUnique() {
+        delete p;
+    }
+    
+    // Получение сырого указателя
+    T* get() const {
+        return p;
+    }
+    
+    // Перегрузка операторов
+    T& operator*() const {
+        return *p;
+    }
+    
+    T* operator->() const {
+        return p;
+    }
+};
+
+// Функция создания MyUnique
+template<typename T, typename... Args>
+MyUnique<T> Make_MyUnique(Args&&... args) {
+    return MyUnique<T>(new T(forward<Args>(args)...);
 }
 
-void loadFromFile(const string& filename, Groups& outGroups) {
-    ifstream file(filename);
-    string groupName;
-    while (getline(file, groupName)) {
-        int studentCount;
-        file >> studentCount;
-        file.ignore();
-        
-        vector<Student> students;
-        for (int i = 0; i < studentCount; i++) {
-            Student student;
-            getline(file, student.Name);
-            file >> student.Year;
-            file.ignore();
-            
-            int recordCount;
-            file >> recordCount;
-            file.ignore();
-            
-            for (int j = 0; j < recordCount; j++) {
-                string subject;
-                int score;
-                getline(file, subject);
-                file >> score;
-                file.ignore();
-                student.RecordBook[subject] = static_cast<Score>(score);
-            }
-            students.push_back(student);
+// Часть 2: Реализация shared_ptr
+template<class T>
+class MyShared {
+    T* p = nullptr;
+    size_t* count = nullptr;
+
+    void release() {
+        if (count && --(*count) == 0) {
+            delete p;
+            delete count;
         }
-        outGroups[groupName] = students;
+        p = nullptr;
+        count = nullptr;
     }
+
+public:
+    // Конструктор с сырым указателем
+    explicit MyShared(T* ptr = nullptr) : p(ptr), count(ptr ? new size_t(1) : nullptr) {}
+    
+    // Копирование
+    MyShared(const MyShared& other) : p(other.p), count(other.count) {
+        if (count) ++(*count);
+    }
+    
+    MyShared& operator=(const MyShared& other) {
+        if (this != &other) {
+            release();
+            p = other.p;
+            count = other.count;
+            if (count) ++(*count);
+        }
+        return *this;
+    }
+    
+    // Перемещение
+    MyShared(MyShared&& other) noexcept : p(other.p), count(other.count) {
+        other.p = nullptr;
+        other.count = nullptr;
+    }
+    
+    MyShared& operator=(MyShared&& other) noexcept {
+        if (this != &other) {
+            release();
+            p = other.p;
+            count = other.count;
+            other.p = nullptr;
+            other.count = nullptr;
+        }
+        return *this;
+    }
+    
+    // Деструктор
+    ~MyShared() {
+        release();
+    }
+    
+    // Получение сырого указателя
+    T* get() const {
+        return p;
+    }
+    
+    // Перегрузка операторов
+    T& operator*() const {
+        return *p;
+    }
+    
+    T* operator->() const {
+        return p;
+    }
+    
+    // Количество ссылок
+    size_t use_count() const {
+        return count ? *count : 0;
+    }
+};
+
+// Функция создания MyShared
+template<typename T, typename... Args>
+MyShared<T> Make_MyShared(Args&&... args) {
+    return MyShared<T>(new T(forward<Args>(args)...);
 }
+
+// Тестовый класс для демонстрации
+class MyPoint {
+    int x, y;
+public:
+    MyPoint(int x, int y) : x(x), y(y) {
+        cout << "MyPoint constructed: " << x << ", " << y << endl;
+    }
+    ~MyPoint() {
+        cout << "MyPoint destroyed: " << x << ", " << y << endl;
+    }
+    void print() const {
+        cout << "Point: (" << x << ", " << y << ")" << endl;
+    }
+};
 
 int main() {
-    vector<string> words = {"word1", "word2", "word3"};
-    saveToFile("words.txt", words);
+    // Демонстрация MyUnique
+    {
+        cout << "=== MyUnique Demo ===" << endl;
+        auto p1 = Make_MyUnique<MyPoint>(1, 2);
+        p1->print();
+        
+        // MyUnique<MyPoint> p2 = p1; // Ошибка: копирование запрещено
+        MyUnique<MyPoint> p3 = move(p1); // Перемещение разрешено
+        if (!p1.get()) cout << "p1 is now empty" << endl;
+        p3->print();
+    }
     
-    vector<Book> books = {{"Author1", "Title1", 2000}, {"Author2", "Title2", 2005}};
-    saveToFile("books.txt", books);
-    
-    Groups groups;
-    Student s1 = {"Ivan", 2020, {{"Math", Excellent}, {"Physics", Good}}};
-    Student s2 = {"Maria", 2021, {{"Math", Good}, {"Chemistry", Satisfactorily}}};
-    groups["Group1"] = {s1, s2};
-    saveToFile("groups.txt", groups);
+    // Демонстрация MyShared
+    {
+        cout << "\n=== MyShared Demo ===" << endl;
+        auto p1 = Make_MyShared<MyPoint>(3, 4);
+        cout << "p1 use count: " << p1.use_count() << endl;
+        
+        auto p2 = p1; // Копирование разрешено
+        cout << "p1 use count: " << p1.use_count() << endl;
+        cout << "p2 use count: " << p2.use_count() << endl;
+        
+        p1->print();
+        p2->print();
+        
+        auto p3 = move(p2); // Перемещение
+        cout << "After move:" << endl;
+        cout << "p1 use count: " << p1.use_count() << endl;
+        cout << "p2 use count: " << p2.use_count() << endl;
+        cout << "p3 use count: " << p3.use_count() << endl;
+    }
     
     return 0;
 }
